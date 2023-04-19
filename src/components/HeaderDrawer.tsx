1ef508a978395c8676ff359623b23f6e3e715c3f
rodrigo.baggio.si@gmail.com <Baggio25>
2023-04-19 17:08:53 -0300

    Aula 16

--- src/components/HeaderDrawer.tsx
+++ src/components/HeaderDrawer.tsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Drawer, Typography } from "@mui/material"
+import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material"
+import Link from "next/link";
 
 type Props = {
     open: boolean;
@@ -31,6 +32,34 @@ export const HeaderDrawer = ({onClose, onLogout, open, title}: Props) => {
                     {title}
                 </Typography>
                 <Divider />
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <Link href="/pedidos" style={{ color: "#000", textDecoration: "none"}}>
+                                <ListItemText primary="Pedidos" />
+                            </Link> 
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <Link href="/produtos" style={{ color: "#000", textDecoration: "none"}}>
+                                <ListItemText primary="Produtos" />
+                            </Link>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <Link href="/categorias" style={{ color: "#000", textDecoration: "none"}}>
+                                <ListItemText primary="Categorias" />
+                            </Link>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={onLogout}>
+                            <ListItemText primary="Sair" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
             </Box>
         </Drawer>
     )
