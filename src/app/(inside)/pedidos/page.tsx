720802949eb0f1d5fa5c2d7b89c6aae0a63ea66e
rodrigo.baggio.si@gmail.com <Baggio25>
2023-04-20 07:54:06 -0300

    Aula 17

--- src/app/(inside)/pedidos/page.tsx
+++ src/app/(inside)/pedidos/page.tsx
@@ -1,11 +1,65 @@
 "use client";
 
-import { Box } from "@mui/material"
+import { useState } from "react";
+import { Box, Button, InputAdornment, TextField, Typography } from "@mui/material"
+import { Search, Refresh } from "@mui/icons-material";
 
 const Page = () => {
+    const [searchInput, setSearchInput] =  useState("");
+
+    const handleSearchInput = () => {
+
+    }
+
+    const handleSearchKey = () => {
+
+    }
+
     return(
-        <Box>
-            Pedidos
+        <Box sx={{ my: 3 }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box sx={{ display: "flex", alignItems: "center"}}>
+                    <Typography 
+                        component="h5" 
+                        variant="h5"
+                        sx={{ color: "#555", mr: 2 }}
+                    >
+                        Pedidos
+                    </Typography>
+                    <Button 
+                        sx={{ justifyContent: { xs: "flex-start" } }}
+                        size="small"
+                    >
+                        <Refresh />
+                        <Typography
+                            component="div"
+                            sx={{
+                                color: "#555", 
+                                display: {
+                                    xs: "none", 
+                                    sm: "block"
+                                }
+                            }}
+                        >
+                            Atualizar
+                        </Typography>
+                    </Button>
+                </Box>
+                <TextField
+                    value={searchInput}
+                    onChange={handleSearchInput}
+                    onKeyUp={handleSearchKey}
+                    placeholder="Pesquise um pedido..."
+                    variant="standard"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Search />
+                            </InputAdornment>
+                        )
+                    }}
+                />
+            </Box>
         </Box>
     )
 }
