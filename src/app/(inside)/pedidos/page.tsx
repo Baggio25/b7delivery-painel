63b652a854212008c371533c94bb9b3b6232901f
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-04-25 22:20:07 -0300

    Aula 27

--- src/app/(inside)/pedidos/page.tsx
+++ src/app/(inside)/pedidos/page.tsx
@@ -14,6 +14,7 @@ const Page = () => {
     const [loading, setLoading] = useState(false);
     const [orders, setOrders] = useState<Order[]>([]);
     const [filteredOrders, setFilteredOrders] = useState<Order[]>([]);
+    const [printOrder, setPrintOrder] = useState<Order | null>(null);
 
     const getOrders = async () => {
         setSearchInput("");
@@ -58,93 +59,106 @@ const Page = () => {
         getOrders;
     }
 
+    const handlePrintAction = (order: Order) => {
+        setPrintOrder(order);
+        setTimeout(() => {
+            if(window) window.print();
+        }, 200);
+    }
+
     return(
-        <Box sx={{ my: 3 }}>
-            <Box sx={{ mb: 3, display: "flex", justifyContent: "space-between" }}>
-                <Box sx={{ display: "flex", alignItems: "center"}}>
-                    <Typography 
-                        component="h5" 
-                        variant="h5"
-                        sx={{ color: "#555", mr: 2 }}
-                    >
-                        Pedidos
-                    </Typography>
-                    {loading && <CircularProgress size={24}/>}
-                    {!loading && 
-                        <Button 
-                            sx={{ justifyContent: { xs: "flex-start" } }}
-                            size="small"
-                            onClick={getOrders}
+        <>
+            <Box sx={{ my: 3, displayPrint: "none" }}>
+                <Box sx={{ mb: 3, display: "flex", justifyContent: "space-between" }}>
+                    <Box sx={{ display: "flex", alignItems: "center"}}>
+                        <Typography 
+                            component="h5" 
+                            variant="h5"
+                            sx={{ color: "#555", mr: 2 }}
                         >
-                            <Refresh />
-                            <Typography
-                                component="div"
-                                sx={{
-                                    color: "#555", 
-                                    display: {
-                                        xs: "none", 
-                                        sm: "block"
-                                    }
-                                }}
+                            Pedidos
+                        </Typography>
+                        {loading && <CircularProgress size={24}/>}
+                        {!loading && 
+                            <Button 
+                                sx={{ justifyContent: { xs: "flex-start" } }}
+                                size="small"
+                                onClick={getOrders}
                             >
-                                Atualizar
-                            </Typography>
-                        </Button>
-                    }
+                                <Refresh />
+                                <Typography
+                                    component="div"
+                                    sx={{
+                                        color: "#555", 
+                                        display: {
+                                            xs: "none", 
+                                            sm: "block"
+                                        }
+                                    }}
+                                >
+                                    Atualizar
+                                </Typography>
+                            </Button>
+                        }
+                    </Box>
+                    <TextField
+                        value={searchInput}
+                        onChange={e => setSearchInput(e.target.value)}
+                        onKeyUp={handleSearchKey}
+                        placeholder="Pesquise um pedido..."
+                        variant="standard"
+                        disabled={loading}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <Search />
+                                </InputAdornment>
+                            )
+                        }}
+                    />
                 </Box>
-                <TextField
-                    value={searchInput}
-                    onChange={e => setSearchInput(e.target.value)}
-                    onKeyUp={handleSearchKey}
-                    placeholder="Pesquise um pedido..."
-                    variant="standard"
-                    disabled={loading}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Search />
-                            </InputAdornment>
-                        )
+
+                <Grid 
+                    container 
+                    spacing={3} 
+                    columns={{
+                        xs: 1, 
+                        sm: 2,
+                        md: 4
                     }}
-                />
+                >
+                    {loading &&
+                        <>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <Skeleton variant="rectangular" height={220} />
+                            </Grid>
+                        </>
+                    }
+
+                    {!loading && filteredOrders.map((item, index) => (
+                        <Grid key={index} item xs={1}>
+                            <OrderItem 
+                                item={item}
+                                onChangeStatus={handleChangeStatus}    
+                                onPrint={handlePrintAction}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
             </Box>
-
-            <Grid 
-                container 
-                spacing={3} 
-                columns={{
-                    xs: 1, 
-                    sm: 2,
-                    md: 4
-                }}
-            >
-                {loading &&
-                    <>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Skeleton variant="rectangular" height={220} />
-                        </Grid>
-                    </>
-                }
-
-                {!loading && filteredOrders.map((item, index) => (
-                    <Grid key={index} item xs={1}>
-                        <OrderItem 
-                            item={item}
-                            onChangeStatus={handleChangeStatus}    
-                        />
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
+            <Box sx={{ display: "none", displayPrint: "block" }}>
+                ...
+            </Box>
+        </>
     )
 }
 
